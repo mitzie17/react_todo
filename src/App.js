de48bff2b05308ea3f43bfb63e331c57a8b53b24
Mitzi Estrada <mitzie@Mitzis-Laptop.lan>
2025-06-30 22:18:22 -0700

    import Form component and render it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header>Mitzi's Todo List</header>
+      <Form />
     </div>
   );
 }
